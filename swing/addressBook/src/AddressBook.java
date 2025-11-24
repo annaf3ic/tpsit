9d2e44bd3ca4c8ffe028103a8fa1cb04145a03b7
annaf3ic <anna.ficotto@itiszuccante.edu.it>
2025-11-24 16:41:18 +0100

    Ristrutturato AddressBook: aggiunta gestione contatti e migliorata visibilità della lista

--- swing/addressBook/src/AddressBook.java
+++ swing/addressBook/src/AddressBook.java
@@ -1,11 +1,37 @@
 import java.util.ArrayList;
 
 public class AddressBook {
-    ArrayList<Contact> book;
+    private ArrayList<Contact> book;
 
     public AddressBook() {
         book = new ArrayList<>();
     }
 
-    public void add()
+    public void addContact(Contact c) {
+        book.add(c);
+    }
+
+    public void removeContact(Contact c) {
+        book.remove(c);
+    }
+
+    public Contact searchByName(String name) {
+        for (Contact c : book) {
+            if (c.getName().equalsIgnoreCase(name)) {
+                return c;
+            }
+        }
+        return null;
+    }
+
+    public ArrayList<Contact> getAllContacts() {
+        return book;
+    }
+
+    public void updateContact(Contact oldC, Contact newC) {
+        int index = book.indexOf(oldC);
+        if (index != -1) {
+            book.set(index, newC);
+        }
+    }
 }
