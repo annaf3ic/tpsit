6e3c955ebe60d502b12426cec70c3769e12eed26
annaf3ic <anna.ficotto@itiszuccante.edu.it>
2025-11-24 16:42:51 +0100

    modificato Main

--- swing/addressBook/src/Main.java
+++ swing/addressBook/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        AddressBook book = new AddressBook();
+        javax.swing.SwingUtilities.invokeLater(() -> {new GraphicBook(book);});
     }
 }
