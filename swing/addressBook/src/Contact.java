08db6fabb45064ff8f446a3a0ed495976540474d
annaf3ic <anna.ficotto@itiszuccante.edu.it>
2025-11-24 16:42:14 +0100

    Classe Contact aggiornata: aggiunto campo indirizzo e costruttore, getter e setter modificati

--- swing/addressBook/src/Contact.java
+++ swing/addressBook/src/Contact.java
@@ -5,9 +5,28 @@ public class Contact {
     String surname;
     String email;
     String mobile;
+    String address;
     Date birth;
     
-    public Contact(String name, String surname, String email, String mobile, Date birth) {
+    public Contact(String name, String surname, String email, String mobile, String address) {
         this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.mobile = mobile;
+        this.address = address;
     }
+
+    public String getName() {return name;}
+    public String getSurname() {return surname;}
+    public String getEmail() {return email;}
+    public String getMobile() {return mobile;}
+    public String getAddress() {return address;}
+    public Date getBirth() {return birth;}
+
+    public void setName(String name) {this.name = name;}
+    public void setSurname(String surname) {this.surname = surname;}
+    public void setMobile(String mobile) {this.mobile = mobile;}
+    public void setEmail(String email) {this.email = email;}
+    public void setAddress(String address) {this.address = address;}
+    public void setBirth(Date birth) {this.birth = birth;}
 }
