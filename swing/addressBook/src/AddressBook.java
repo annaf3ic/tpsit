2d15e798d6dd899b55d6330e7b649fe0b0596705
annaf3ic <anna.ficotto@itiszuccante.edu.it>
2025-11-25 14:37:35 +0100

    Iniziata versione con csv

--- swing/addressBook/src/AddressBook.java
+++ swing/addressBook/src/AddressBook.java
@@ -1,10 +1,15 @@
 import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
 
 public class AddressBook {
     private ArrayList<Contact> book;
 
     public AddressBook() {
         book = new ArrayList<>();
+        String file = "./data.csv"; //file path
     }
 
     public void addContact(Contact c) {
@@ -32,6 +37,43 @@ public class AddressBook {
         int index = book.indexOf(oldC);
         if (index != -1) {
             book.set(index, newC);
+        }
+    }
+
+    private void save(String fileName) {
+        try {
+            FileWriter o = new FileWriter(fileName);
+            BufferedWriter out = new BufferedWriter(o);
+            String s;
+            for (Contact c : book) {
+                s = String.join(",", c.getName(), c.getSurname(), c.getEmail(), c.getMobile(), c.getAddress());
+                out.write(s);
+                out.newLine();
+                out.flush();
+            }
+            o.close();
+            out.close();
+        } catch (Exception e) {
+            System.out.println("Errore in scrittura del file");
+        }
+    }
+
+    private void importFromFiles(String fileName) {
+        try {
+            FileReader i = new FileReader(fileName);
+            BufferedReader in = new BufferedReader(i);
+            String s;
+            while ((s = in.readLine()) != null) {
+                String[] strings = s.split(",");
+                for (String s : strings) {
+                    System.out.println(s);
+                }
+                in.readLine();
+            }
+            i.close();
+            in.close();
+        } catch (Exception e) {
+            System.out.println("Errore in lettura del file");
         }
     }
 }
